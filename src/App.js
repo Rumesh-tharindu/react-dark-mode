d4533bebd08d529fe0e265222243e8dfde970803
Rumesh Tharindu <59620214+Rumesh-tharindu@users.noreply.github.com>
2021-02-03 22:22:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import React from "react";
+export default function App() {
+  return (
+    <React.Fragment>
+      <h1>
+        <div>count1 you are awesome but</div>
+      </h1>
+    </React.Fragment>
+  );
+}
